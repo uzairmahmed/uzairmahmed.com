9a24ac2cb3420cb4264b9d125f5bc06278231c29
Uzair Ahmed <uzairmahmed@gmail.com>
2020-09-10 13:30:30 -0400

    form testing

--- frontend/src/common/ContactForm.js
+++ frontend/src/common/ContactForm.js
@@ -8,7 +8,7 @@ import { baseStyles, contactStyles } from '../styles'
 class ContactForm extends Component {
   render() {
     return (
-      <Form style={contactStyles.card} >
+      <Form data-netlify="true" style={contactStyles.card} >
         <Form.Group controlId="formText">
           <Form.Control
             required
